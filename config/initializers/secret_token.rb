cef3b52b9f7e7210c967cf477f51783504ebe1af
Omar <ovargas27@gmail.com>
2011-01-19 18:44:24 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tacomanager::Application.config.secret_token = 'f7fef7557403f4477ce6389944182d8ff2eabd580385beca62fe8746cab9e4f4fd8fd3d9a04fae1e97b66dccd1161410a70c4588b8ce6c79b5001b1b72c0b657'
---
+++ features/basic.feature
@@ -0,0 +1,8 @@
+Feature: Hello World Feature
+  In order to ensure that my installation works
+  AS a Developer
+  I want to run a quick Cucumber test
+
+Scenario: Hello World Scenario
+    Given I am on the home page 
+    Then I should see "I wanna taco. And you?"
